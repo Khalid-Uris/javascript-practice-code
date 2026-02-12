ca5b8166ce6625da998c729341147fe12a50f8d6
Khalid Uris <68448181+Khalid-Uris@users.noreply.github.com>
2026-02-12 12:07:52 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3); // we are using nodejs, not browser
+
+// console.log("hello sworld!");
+
+let isLoggedIn = false;
+let state;
+
+// Premetive data type
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+// console.log(typeof "khan");
+// console.log(typeof 5);
+// console.log(typeof null); // object
+// console.log(typeof undefined); is type undefined
