6dd8279541405f68bd3f0bbcd687c82f4cda1df9
Khalid Uris <68448181+Khalid-Uris@users.noreply.github.com>
2026-02-14 12:51:19 +0000

    conversions operation in js

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,41 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+let score = "khan";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+
+//Boolean Convertion
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "khan";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "khan" => true
+
+
+// String Convertion
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
