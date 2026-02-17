58e3a1655393fb2911c718357e1e0fc0efad5d13
Khalid Uris <68448181+Khalid-Uris@users.noreply.github.com>
2026-02-17 15:13:45 +0000

    string and Operation

--- 01_basics/03_conversionsOperation.js
+++ 01_basics/03_conversionsOperation.js
@@ -37,5 +37,54 @@ let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+// ********************** Operation **************************
+
+let value = 3;
+let negValue = -value
+
+console.log(negValue);
+
+
+// console.log(2 + 2);
+// console.log(2 * 2);
+// console.log(2 - 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Hello";
+let str2 = " khan";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(true + true); // 2
+// console.log(true + false); // 1
+// console.log(false + false); // 0
+
+// console.log(+true); // 1
+// console.log(+false); // 0
+// console.log(true+); // error
+// console.log(+""); // 0
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100;
+//gameCounter++; // gameCounter = gameCounter + 1 and gameCounter += 1;
+++gameCounter;
+console.log(gameCounter);
